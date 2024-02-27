f292da98deb31823aded0158c51c838642b97fcb
Binod Aryal <binodaryal21@gmail.com>
2024-02-27 19:34:25 +0545

    fix: next-sitemap.config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://konstantin.digital',
+  siteUrl: 'https://productcraft.io',
   generateRobotsTxt: true,
   exclude: ['/imprint', '/transparency', '/404'],
 };
