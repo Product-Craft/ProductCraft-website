10181fcf28e1ffccfbaff77d82b1269ae84b9353
Binod Aryal <binodaryal21@gmail.com>
2024-03-07 00:03:31 +0545

    Update: About tsx, pic upload

--- app/_sections/About.tsx
+++ app/_sections/About.tsx
@@ -10,30 +10,29 @@ export const AboutSection = () => {
             Hey, I am Binod
           </h3>
           <p className="my-5 text-xl md:text-2xl">
-            A freelance product engineer - or to put it simply: a product
-            manager who codes.
+            A solution engineer - or to put it simply: A technical architect, 
+            crafting and implementing tailored solutions to resolve complex business challenges..
           </p>
           <p className="my-5 opacity-90">
-            My passion has always been at the intersection of product and web
-            development. I love talking to users and developing ideas as much as
-            coding with a good lo-fi playlist running in the back 🎧
+          My passion lies in the fusion of product innovation and user engagement. 
+          I enjoy conversing with users, brainstorming innovative concepts, and crafting solutions. 
           </p>
           <div className="mt-10 flex flex-col gap-4 sm:flex-row">
-            <Label color="gray">5+ years of building web apps</Label>
-            <Label color="gray">educator at techlabs.org</Label>
+            <Label color="gray">5+ years of building apps</Label>
+            <Label color="gray">PM & Pre Sales Engineer at webpoint.io</Label>
           </div>
         </div>
         <div className="max-w-[580px] flex-shrink-0 rounded-md">
           <Image
-            src="/images/portraits/konstantin-muenster-wide-couch.jpg"
-            alt="Konstantin Münster"
+            src="/images/portraits/binod-aryal-crosshand-smile.jpg"
+            alt="Binod Aryal"
             width={480 * 2}
             height={324 * 2}
             sizes="(max-width: 768px) 100vw, 760px"
             className="h-full w-full rounded-md object-cover"
           />
           <span className="mt-2 hidden text-center text-xs opacity-50 md:block">
-            Building web apps since high school. Waiting for yours since.
+          From ideation to launch and sustainment, I cover every aspect of the product lifecycle, focusing on efficiency and effectiveness.
           </span>
         </div>
       </div>
